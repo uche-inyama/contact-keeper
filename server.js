4c1d42852d4ef1d2eed6199f2f1cf3c243f15b48
uche-inyama <depoulet@yahoo.com>
2021-11-26 11:12:27 -0500

    setting mongoose for connection MongodbAtlas

--- server.js
+++ server.js
@@ -1,15 +1,25 @@
 const express = require('express');
-
-const app = express();
-
-app.get('/', (req, res) => res.json({
-  msg: 'welcome...'
-}))
-
-app.use('/api/users', require('./routes/users'));
-app.use('/api/auth', require('./routes/auth'));
-app.use('/api/contacts', require('./routes/contacts'));
-
+const mongoose = require('mongoose');
 const PORT = process.env.PORT || 4000;
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+// Connect to Database
+
+mongoose.connect('mongodb://localhost:27017/express-mongoose', {
+    useNewUrlParser: true
+  })
+  .then(() => {
+  const app = express();
+
+  app.get('/', (req, res) => res.json({
+    msg: 'welcome...'
+  }))
+  app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+}).catch(() => {
+  console.log('Database connection failed');
+})
+
+// app.use('/api/users', require('./routes/users'));
+// app.use('/api/auth', require('./routes/auth'));
+// app.use('/api/contacts', require('./routes/contacts'));
+
+
