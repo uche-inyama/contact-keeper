e137949ca9b3811a9a323d79cbd73548eb675323
uche-inyama <depoulet@yahoo.com>
2021-11-29 04:20:06 -0500

    configure mongodb database with mongoose

--- config/db.js
+++ config/db.js
@@ -1,4 +1,19 @@
-const express = require('express');
 const mongoose = require('mongoose');
+const config = require('config');
+const db = config.get('mongoURI');
 
-mongoose.connect('mongodb://localhost:3000/express-mongoose')
+const connectDB = async () => {
+	try {
+		await mongoose
+		.connect(db, {
+			useNewUrlParser: true, 
+			useUnifiedTopology: true
+		});
+		console.log('MongoDB Connected');
+	} catch (error) {
+		console.error(err.message);
+		process.exit(1);
+	}
+};
+
+module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,25 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const PORT = process.env.PORT || 4000;
+const connectDB = require('./config/db');
 
-// Connect to Database
+connectDB();
 
-mongoose.connect('mongodb://localhost:27017/express-mongoose', {
-    useNewUrlParser: true
-  })
-  .then(() => {
-  const app = express();
+const app = express();
 
-  app.get('/', (req, res) => res.json({
-    msg: 'welcome...'
-  }))
-  app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
-}).catch(() => {
-  console.log('Database connection failed');
-})
+app.get('/', (req, res) => res.json({
+  msg: 'welcome...'
+}))
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 
-// app.use('/api/users', require('./routes/users'));
-// app.use('/api/auth', require('./routes/auth'));
-// app.use('/api/contacts', require('./routes/contacts'));
-
-
+app.use('/api/users', require('./routes/users'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/contacts', require('./routes/contacts'));
