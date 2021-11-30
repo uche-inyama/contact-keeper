54a1cad9b79bbd4207b59218000123f510d3a891
uche-inyama <depoulet@yahoo.com>
2021-11-30 04:45:04 -0500

    add middleware for secure login with jwtwebtoken

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
-const User = require('../models/User');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const { body, validationResult } = require('express-validator');
+const User = require('../models/User');
 
 // @route    POST api/users
 // @desc     Register a user
@@ -36,6 +38,22 @@ router.post('/', [
      const salt = await bcrypt.genSalt(10);
      user.password = await bcrypt.hash(password, salt);
      await user.save();
+
+     const payload = {
+       user: {
+         id: user.id
+       }
+     }
+
+     jwt.sign(payload, config.get('jwtSecret'), 
+        {
+          expiresIn: 360000
+        }, 
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token })
+        }
+      )
    } catch (err) {
       console.error(err.message);
       res.status(500).send('Server error'); 
