ea2844eacf2fe347a2ca3cb680e3f83f99cef5b7
uche-inyama <depoulet@yahoo.com>
2021-12-01 11:04:44 -0500

    setup context for contact

--- client/src/components/contacts/contact.js
+++ client/src/components/contacts/contact.js
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { Fragment, useContext } from 'react';
+import ContactContext from '../../context/contact/contactContext';
 
 const Contacts = () => {
+  const contactContext = useContext(ContactContext);
+
+  const { contacts } = contactContext;
+
   return (
-    <div>
-        
-    </div>
+    <Fragment>
+      {contacts.map(contact => (
+        <h3>{contact.name}</h3>
+      ))}
+    </Fragment>
   )
 }
 
